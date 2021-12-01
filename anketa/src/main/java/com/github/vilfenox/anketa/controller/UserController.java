c95504892b59100f1f743c4d9151e0e2286add4e
VilFenoX <68443337+VilFenoX@users.noreply.github.com>
2021-12-01 22:59:43 +0400

    1/12/21

--- anketa/src/main/java/com/github/vilfenox/anketa/controller/UserController.java
+++ anketa/src/main/java/com/github/vilfenox/anketa/controller/UserController.java
@@ -6,8 +6,13 @@ import com.github.vilfenox.anketa.Entity.Questions;
 import com.github.vilfenox.anketa.Entity.Variants;
 import com.github.vilfenox.anketa.repository.QuestionnairesRepository;
 import com.github.vilfenox.anketa.repository.QuestionsRepository;
+import com.github.vilfenox.anketa.repository.UserRepository;
 import com.github.vilfenox.anketa.repository.VariantsRepository;
+import com.github.vilfenox.anketa.security.SecurityUser;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
@@ -24,6 +29,8 @@ public class UserController {
     private QuestionsRepository questionsRepository;
     @Autowired
     private VariantsRepository variantsRepository;
+    @Autowired
+    private UserRepository userRepository;
 
     @GetMapping
     public String getAllQuestionnaires(Model model){
@@ -35,8 +42,16 @@ public class UserController {
     @GetMapping("/questionnaire/{id}")
   //  @PreAuthorize("hasAuthority('developers:read')")
     public String listQuestion(@PathVariable Long id, Model model){
+
+            // получаем авторизированного пользователя
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        String currentPrincipalName = authentication.getName();
+
         Questionnaires questionnaire = questionnairesRepository.findById(id).get();
         model.addAttribute("questionnaire", questionnaire);
+
+        model.addAttribute("user", userRepository.findByEmail(currentPrincipalName));
+
         Iterable<Questions> questionsFromBD = questionsRepository.findAllByQuestionnaire_Id(id);
         model.addAttribute("questions", questionsFromBD);
         return "/user_variants";
